c110458608a77a14f99a73a729fcc78560e8bffd
RajnishPuri <rajnishpuri15@gmail.com>
2025-01-08 17:27:32 +0530

    Frontend done

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -5,13 +5,45 @@ import { useNavigate } from "react-router-dom";
 import CartItem from "../Component/CartItem";
 import { useDispatch } from "react-redux";
 import EmptyCart from "../Component/EmptyCart";
+import {loadStripe} from '@stripe/stripe-js';
+
 
 export default function Cart() {
     const Navigate = useNavigate();
     const { cart } = useSelector((state) => state);
 
     const [items, setItems] = useState(cart.length);
+    const [email, setEmail] = useState("");
 
+    async function checkoutHandler(){
+        const stripe = await loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+
+        const body = {
+            products:cart,
+            email:email
+        }
+
+        const headers = {
+            "Content-Type":"application/json"
+        }
+
+        const response = await fetch("http://localhost:3000/create-checkout-session", {
+            method:"POST",
+            headers:headers,
+            body:JSON.stringify(body)
+        })
+
+        const session = await response.json();
+
+        const result = stripe.redirectToCheckout({
+            sessionId:session.id
+        })
+
+        if(result.error){
+            console.log(result.error)
+        }
+
+    }
 
     if (items === 0) {
         return (
@@ -43,7 +75,8 @@ export default function Cart() {
                             </div>
                             <div className="flex flex-col">
                                 <p className="text-xl font-bold">Total Amount : ${totalPrice}</p>
-                                <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">CHECKOUT NOW</button>
+                                <input type="email" placeholder="Enter your email" className="p-2 border-2 border-gray-400 rounded-lg" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl" onClick={checkoutHandler}>CHECKOUT NOW</button>
                             </div>
 
                         </div>
